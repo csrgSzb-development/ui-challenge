e8c78c803d1d485558236575d81fd9c4ca7f3666
csurgoSzabi <csurgo.szabolcs@progmasters.hu>
2023-04-08 10:19:28 +0200

    add user, article models

---
+++ frontend/src/app/models/create-article.ts
@@ -0,0 +1,7 @@
+export interface CreateArticle {
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+}
+
---
+++ frontend/src/app/models/update-user.ts
@@ -0,0 +1,6 @@
+export interface UpdateUser {
+  username: string;
+  email: string;
+  bio: string;
+  image: string;
+}
